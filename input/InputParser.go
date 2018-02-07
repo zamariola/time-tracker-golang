e3351b6875942e0a67b9b8f87386dc7862f5ca52
Leonardo Zamariola <leonardo.zamariola@gmail.com>
2018-02-07 09:17:21 -0200

    Inserting some date time text parse logic

--- input/InputParser.go
+++ input/InputParser.go
@@ -4,14 +4,19 @@ import (
 	"time"
 	"errors"
 	"fmt"
+	"regexp"
+	"github.com/zamariola/time-tracker-golang/util"
 )
 
 const (
 	ARGS_INDEX_MESSAGE = 0;
-	ARGS_INDEX_START_DATE = 1;
 	ARGS_INDEX_START_TIME = 2;
-	ARGS_INDEX_END_DATE = 3;
 	ARGS_INDEX_END_TIME = 4;
+
+	SHORTCUT_LETTER_NOW = "n";
+	SHORTCUT_LETTER_LAST = "l";
+	SHORTCUT_LETTER_YESTERDAY = "y";
+	SHORTCUT_LETTER_TODAY = "t";
 )
 
 type Task struct {
@@ -26,17 +31,71 @@ func ParseArgs(args []string) (*Task, error) {
 		return &Task{}, errors.New(fmt.Sprintf("Invalid parameters length, expected 5 received %d", len(args)))
 	}
 
-	return parseArgsContent(args);
+	return parseArgsContent(args), nil;
 }
 
-func parseArgsContent(args []string) (*Task, error) {
+func parseArgsContent(args []string) *Task {
 
 	task := Task{}
-	task.message = args[ARGS_INDEX_MESSAGE]
+	var err error = nil
+
+	for i := range args {
+
+		switch i {
+		case ARGS_INDEX_MESSAGE:
+			task.message = args[i];
+		case ARGS_INDEX_START_TIME:
+			task.start, err = parseDateTime(args[i - 1], args[i]);
+			util.CheckError(err);
+		case ARGS_INDEX_END_TIME:
+			task.end, err = parseDateTime(args[i - 1], args[i]);
+			util.CheckError(err)
+		}
+	}
+
+	return &task;
+
+}
+func parseDateTime(dateString string, timeString string) (time.Time, error) {
+
+	alphaNumPattern := regexp.MustCompile(`^[A-Za-z]+$`)
+	var day, hour time.Time
 
 	//TODO: Parse start and end datetimes using letters to short (today, now, yesterday and etc)
-	task.start,_ = time.Parse("Mon Jan 2 15:04:05 -0700 MST 2006",args[ARGS_INDEX_START_DATE])
+	if alphaNumPattern.MatchString(dateString) {
+		day = convertShortcutToDateTime(dateString);
+	} else {
+		//TODO: Insert logic here
+		day, _ = time.Parse("Mon Jan 2 15:04:05 -0700 MST 2006", dateString)
+	}
+	if alphaNumPattern.MatchString(dateString) {
+		hour = convertShortcutToDateTime(timeString);
+	} else {
+		//TODO: Insert logic here
+	}
 
-	return &task,nil
+	//TODO: Combine this two infos
+	fmt.Print(day);
+	fmt.Print(hour);
 
+	return *new(time.Time), nil;
+}
+
+func convertShortcutToDateTime(text string) time.Time {
+
+	switch text {
+
+	case SHORTCUT_LETTER_NOW:
+		return time.Now();
+	case SHORTCUT_LETTER_LAST:
+		return *new(time.Time);
+	case SHORTCUT_LETTER_YESTERDAY:
+		dur, err := time.ParseDuration("-24h");
+		util.CheckError(err);
+		return time.Now().Add(dur);
+	case SHORTCUT_LETTER_TODAY:
+		return time.Now();
+	default:
+		return time.Now();
+	}
 }
